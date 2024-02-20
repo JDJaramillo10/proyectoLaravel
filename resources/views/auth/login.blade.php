7cf2df5487039298d5e2599181e1dab1496e8d0a
JuanRoVaz18 <153143554+JuanRoVaz18@users.noreply.github.com>
2024-02-20 08:47:28 -0500

    login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -63,6 +63,25 @@
                                     </a>
                                 @endif
                             </div>
+
+                            <div class="row mb-3">
+    <label for="tipoUsuario" class="col-md-4 col-form-label text-md-end">{{ __('User Type') }}</label>
+
+    <div class="col-md-6">
+        <select id="tipoUsuario" class="form-control" name="tipoUsuario" required>
+            <option value="cliente">Cliente</option>
+            <option value="primario">Primario</option>
+        </select>
+
+        @error('tipoUsuario')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
+    </div>
+</div>
+
+                            
                         </div>
                     </form>
                 </div>
