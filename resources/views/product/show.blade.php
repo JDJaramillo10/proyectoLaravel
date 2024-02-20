2651c40c6a5214bf1b483a0bd0d7ce3504babab6
JuanRoVaz18 <153143554+JuanRoVaz18@users.noreply.github.com>
2024-02-20 01:22:07 -0500

    show.blade.php
    De igual manera se la ha dado formato a la vista, ahora el cliente puede seleccionar los productos y ver cuales son.

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('template_title')
-    {{ $product->name ?? "{{ __('Show') Product" }}
+    {{ $product->name ?? __('Show Product') }}
 @endsection
 
 @section('content')
@@ -10,29 +10,37 @@
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">
-                        <div class="float-left">
-                            <span class="card-title">{{ __('Show') }} Product</span>
-                        </div>
-                        <div class="float-right">
-                            <a class="btn btn-primary" href="{{ route('products.index') }}"> {{ __('Back') }}</a>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div>
+                                <h2 class="card-title">{{ __('Show Product') }}</h2>
+                                <p class="card-subtitle">{{ $product->description }}</p>
+                            </div>
+                            <div>
+                                <a class="btn btn-primary" href="{{ route('products.index') }}">{{ __('Back') }}</a>
+                                <form action="{{ route('cart.add', $product->id) }}" method="POST" class="d-inline">
+                                    @csrf
+                                    <button type="submit" class="btn btn-success">{{ __('Add to Cart') }}</button>
+                                </form>
+                            </div>
                         </div>
                     </div>
-
                     <div class="card-body">
-                        
                         <div class="form-group">
-                            <strong>Descripcion:</strong>
-                            {{ $product->descripcion}}
+                            <label for="descripcion">{{ __('Description') }}:</label>
+                            <p>{{ $product->descripcion }}</p>
                         </div>
                         <div class="form-group">
-                            <strong>Price:</strong>
-                            {{ $product->price}}
+                            <label for="price">{{ __('Price') }}:</label>
+                            <p>{{ $product->price }}</p>
                         </div>
                         <div class="form-group">
-                            <strong>Stock:</strong>
-                            {{ $product->stock}}
+                            <label for="stock">{{ __('Stock') }}:</label>
+                            <p>{{ $product->stock }}</p>
                         </div>
-
+                        <div class="form-group">
+                            <label for="imagen">{{ __('Image') }}:</label>
+                            <img src="{{ asset('storage/' . $product->imagen) }}" alt="{{ $product->nombre }}" class="img-fluid">
+                        </div>
                     </div>
                 </div>
             </div>
